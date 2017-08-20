b0c4f1bea81f76f12ef8edf3e3c8e28599610631
Jayson Virissimo <jayson.virissimo@asu.edu>
2017-08-20 15:58:12 -0700

    Add set message function

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,16 +1,25 @@
 let answer = document.getElementById('answer');
 let attempt = document.getElementById('attempt');
 
+function generateCode(numberOfDigits) {
+  numberOfDigits = numberOfDigits || 4;
+  return Math.random().toString().slice(2, 2 + numberOfDigits);
+}
+
 function guess() {
     let input = document.getElementById('user-guess');
-    //add functionality to guess function here
+
+    if (answer.value == '' || attempt.value == '') {
+      setHiddenFields();
+    }
 }
 
 function setHiddenFields() {
   answer.value = generateCode();
+  attempt.value = 0;
 }
 
-function generateCode(numberOfDigits) {
-  numberOfDigits = numberOfDigits || 4;
-  return Math.random().toString().slice(2, 2 + numberOfDigits);
+function setMessage(message) {
+  let label = document.getElementById('message');
+  label.innerHTML = message;
 }
