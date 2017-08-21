137281b0ba538412615badf9cfc876b26795bac0
Jayson Virissimo <jayson.virissimo@asu.edu>
2017-08-20 20:40:30 -0700

    Add function to get results

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,9 +1,40 @@
 let answer = document.getElementById('answer');
 let attempt = document.getElementById('attempt');
 
+function digitsMatch(first, second) {
+  if (first === second) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 function generateCode(numberOfDigits) {
   numberOfDigits = numberOfDigits || 4;
   return Math.random().toString().slice(2, 2 + numberOfDigits);
+}
+
+function getResults(input) {
+  let answer = document.getElementById('answer').value;
+  let results = document.getElementById('results');
+  let resultElement = '<div class="row"><span class="col-md-6">' + input + '</span><div class="col-md-6">';
+
+  for (let i = 0; i < 4; i++) {
+    let guessDigit = input[i];
+    let answerDigit = answer[i];
+
+    if (guessDigit == answerDigit) {
+      resultElement += '<span class="glyphicon glyphicon-ok"></span>';
+    } else if (answer.includes(guessDigit)) {
+      resultElement += '<span class="glyphicon glyphicon-transfer"></span>';
+    } else {
+      resultElement += '<span class="glyphicon glyphicon-remove"></span>';
+    }
+  }
+
+  resultElement += '</div>';
+  results.innerHTML = resultElement;
+  return digitsMatch(input, answer);
 }
 
 function guess() {
